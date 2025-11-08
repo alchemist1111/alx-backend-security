e7068bd33e1021dfe8b24580a0422b0bb578ee9e
alchemist1111 <emoyoeugine@gmail.com>
2025-11-09 01:35:25 +0300

    Task 1: Add IP Blacklisting

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
@@ -7,3 +8,12 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f"Request from {self.ip_address} at {self.timestamp} for {self.path}"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True, help_text="IPv4 or IPv6 address")
+    reason = models.CharField(max_length=255, null=True, blank=True)  # Add this field if needed
+    blocked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ip_address    
